c4c5215c55f772ed89f32b75494f8472140324e1
PRADO MUÑOZ David <dprado@bilbomatica.es>
2018-12-20 15:21:05 +0100

    MDV-141 - Added code to track the language select by the user on Google Translate

--- static/custom/modules/horizontal/includes/general/CookiesController.js
+++ static/custom/modules/horizontal/includes/general/CookiesController.js
@@ -12,8 +12,27 @@ define(function (require) {
 
     return {
         generateController: function (module, ctrlName) {
+
             return angular.module(module)
                 .controller(ctrlName, function ($scope, $log, $window, $cookies) {
+
+                    angular.element("#google_translate_element a.goog-te-menu-value" ).on('click', function() {
+
+                        var items =  angular.element(angular.element("iframe")[0].contentWindow.document).find("a.goog-te-menu2-item ");
+
+                        items.on('click', function(event) {
+                         var language;
+                            if(event.target.tagName.toLowerCase() === "div" ) {
+                                language =angular.element(event.target).find("span.text")[0].innerHTML;
+                                $log.debug("Selected language to translate by google : "+ language);
+                            }else{
+                                language=event.target.innerHTML;
+                                $log.debug("Selected language to translate by google : "+language);
+                            }
+                            window._paq.push(['trackEvent', 'googleTranslateMenu', 'languageTranslate', language, 11]);
+
+                        });
+                    });
 
                     // Literals
                     $scope.i18n_cookies = require('json!dvt/cookies-disclaimer/i18n');
